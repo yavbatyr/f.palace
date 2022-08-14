65e59858d4d3caee4dbd46d2524643011c5e3ca3
Yavbatyr <gadjimuradov.b.m@mail.ru>
2022-08-14 12:41:51 +0300

    has been created and animated  menu burger

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,9 @@
 $(function(){
-
+    $('.header__menu-item').on('click', function () {
+        $('.header__menu-item').toggleClass('header__menu-item--active');
+        $('.header__menu-burger-wrapper').toggleClass('header__menu-burger-wrapper--active');
+    });
+    $('.header__menu-item').on('click', function () {
+        $('.header__menu-burger-item').toggleClass('header__menu-burger-item--active');
+    });
 })
